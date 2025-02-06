3742ec2fdd630848d80d067a63b63e74d14ceac8
Abubakar <hafizabubakarafzal@gmail.com>
2025-02-06 23:04:53 +0500

    Intro page
    That's the intro page I will change it's background image as soon as possible It not look very pretty at this point I will make it awesome as soon as possible I will done it in great style.

--- pages/components/intro.js
+++ pages/components/intro.js
@@ -1,6 +1,42 @@
+import Image from "next/image";
 import React from "react";
+import codeBlock from '../../public/introBlock.png'
+import { FaGithub } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa6";
+import { HiUsers } from "react-icons/hi2"; 
 
 const Intro = () => {
-    return ( <><div>hello</div></>)
+    return ( <><div className="bg1 py-2">
+            <div className="mx-4 sm:text-center mt-[1rem]">
+                <p className="text-[---tw] sm:text-[22px] font-black  ">Hello, </p>
+                <p className="text-[---tw] sm:text-[22px] font-black  ">This Is <a className="text-[---introtextofabubakar] sm:text-[22px] font-black  cursor-pointer hover:scale-[1.12] duration-[1s]">Abubakar Afzal</a>, </p>
+                <p className="text-[---tw] sm:text-[22px] font-black  ">I&rsquo;m a <p className="text-[---tlogo] sm:text-[22px] font-black cursor-pointer hover:scale-[1.12] duration-[1s] ">web developer</p> </p>
+            </div>
+            <div className="mx-[2rem] mt-[2rem] rounded-[1rem] sm:w-auto sm:h- outline-[---borderofcodeblock] outline cursor-pointer hover:scale-[1.08] duration-[2s] bg-black">
+            <div className="mx-auto mt-[2rem] border-b-2 border-[---borderofcodeblock] ">
+                <div className="flex p-2 ml-2 space-x-4">
+                    <div className="w-[1rem] h-[1rem] rounded-full bg-[---redcircleofcodeblock]"></div>
+                    <div className="w-[1rem] h-[1rem] rounded-full bg-[---pinkcircleofcodeblock]"></div>
+                    <div className="w-[1rem] h-[1rem] rounded-full bg-[---bluecircleofcodeblock]"></div>
+                </div>
+            </div>
+            
+            <div className="w-[250px]">
+               <Image src={codeBlock} alt="codeBlock" width={300} height={300} className="mt-[1rem]"/>
+            </div>
+            </div>
+            <div className="flex space-x-4 mt-[3rem] justify-center">
+                <FaGithub className="text-[---pinkcircleofcodeblock] text-[22px] cursor-pointer hover:scale-[1.2] duration-[0.5s]"/>
+                <AiFillInstagram className="text-[---pinkcircleofcodeblock] text-[22px] cursor-pointer hover:scale-[1.2] duration-[0.5s]"/>
+                <IoLogoWhatsapp className="text-[---pinkcircleofcodeblock] text-[22px] cursor-pointer hover:scale-[1.2] duration-[0.5s]"/>
+                <FaLinkedin className="text-[---pinkcircleofcodeblock] text-[22px] cursor-pointer hover:scale-[1.2] duration-[0.5s]"/>
+            </div>
+            <div className="justify-items-center">
+                <button  className="rounded-[2rem] mb-[2rem] text-[---tw] flex  items-center text-[10px] p-2 px-4 border-[2px] mt-[2rem] border-t-[---pinkcircleofcodeblock] border-l-[---redcircleofcodeblock] border-b-[---bluecircleofcodeblock] border-r-[---redcircleofcodeblock] cursor-pointer hover:scale-[1.12] duration-[1s]">CONTACT ME <HiUsers  className="text-[---tw] text-[13px]  relative ml-2"/></button>
+            </div>
+            
+        </div></>)
 }
 export default Intro;
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,48 +1,55 @@
-import React from "react";
-import { AiOutlineMenuUnfold } from "react-icons/ai";
+import React, { useState } from "react";
+import { RxDropdownMenu } from "react-icons/rx";
 import { RxCross2 } from "react-icons/rx";
 const Navbar = () => {
+    const [displaymenu,hidemenu] = useState(false);
+
+    const showmenu=()=>{
+        hidemenu(!displaymenu)
+    }
     return ( <>
-    <div>
+    <div className="absolute">
         {/* small screen */}
-        <div className=" mx-4">
+        <div className=" t:hidden mx-4">
             <nav className="flex sm:justify-between sm:py-4 ">
                 
-                    <a href="" className="text-[---tlogo] sm:text-[18px] cursor-pointer font-bold ">ABUBAKAR AFZAL</a>
+                    <a  className="text-[---tlogo] sm:text-[18px] cursor-pointer font-bold ">ABUBAKAR AFZAL</a>
                     <div className="sticky space-x-[2rem]  flex flex-col sm:text-[14px] space-y-2 mt-[-10px] ">
                         <a></a>
                         <div className="flex flex-col items-center space-x-[2rem] sm:text-[14px]  space-y-2 mx-[20px]">
                             <a></a>
-                    <AiOutlineMenuUnfold className="text-[---tw] sm:text-[25px] cursor-pointer"/>
-                    <RxCross2 className="text-[---tw] sm:text-[22px] cursor-pointer"/></div>
-                    <div className="sticky space-x-[2rem]  flex flex-col sm:text-[14px] text-center space-y-2">
+                                    <RxDropdownMenu onClick={showmenu} className={`text-[---tw] sm:text-[25px] cursor-pointer ${displaymenu ?  "block  cursor-pointer hover:scale-[1.12] duration-[1s]" :"hidden" } `}/> 
+                    
+                    <RxCross2 onClick={showmenu} className={`text-[---tw] sm:text-[22px] cursor-pointer ${displaymenu ?  "hidden" :"block cursor-pointer hover:scale-[1.12] duration-[1s]" } `}/></div>
+                    <div className={`sticky space-x-[2rem]  flex flex-col sm:text-[14px] text-center space-y-2  ${displaymenu ?  "opacity-0 duration-[1s]" : "opacity-100 bg-black duration-[2s]" }  `}>
                         <a></a>
-                    <a href="" className="text-[---tw] cursor-pointer">About</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Projects</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Skills</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Education</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Experience</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Blogs</a>
+                    <a  className="text-[---tw] cursor-pointer hover:bg-[---pinkcircleofcodeblock]">About</a>
+                    <a  className="text-[---tw] cursor-pointer hover:bg-[---pinkcircleofcodeblock]">Projects</a>
+                    <a  className="text-[---tw] cursor-pointer hover:bg-[---pinkcircleofcodeblock]">Skills</a>
+                    <a  className="text-[---tw] cursor-pointer hover:bg-[---pinkcircleofcodeblock]">Education</a>
+                    <a  className="text-[---tw] cursor-pointer hover:bg-[---pinkcircleofcodeblock]">Experience</a>
+                    <a  className="text-[---tw] cursor-pointer hover:bg-[---pinkcircleofcodeblock]">Blogs</a>
                     </div> </div>
             </nav>
         </div>
 
         {/* big screen */}
-        <div className=" t:mx-4 sm:hidden">
+        <div className="t:block t:mx-4 sm:hidden">
             <nav className="t:flex t:justify-between t:items-center t:py-4 ">
                 
-                    <a href="" className="text-[---tlogo] t:text-[20px] cursor-pointer font-bold ">ABUBAKAR AFZAL</a>
+                    <a  className="text-[---tlogo] t:text-[20px] cursor-pointer font-bold ">ABUBAKAR AFZAL</a>
                     <div className="t:space-x-[2rem]">
-                    <a href="" className="text-[---tw] cursor-pointer">About</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Projects</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Skills</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Education</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Experience</a>
-                    <a href="" className="text-[---tw] cursor-pointer">Blogs</a>
+                    <a  className="text-[---tw] cursor-pointer hover:text-[---pinkcircleofcodeblock]">About</a>
+                    <a  className="text-[---tw] cursor-pointer hover:text-[---pinkcircleofcodeblock]">Projects</a>
+                    <a  className="text-[---tw] cursor-pointer hover:text-[---pinkcircleofcodeblock]">Skills</a>
+                    <a  className="text-[---tw] cursor-pointer hover:text-[---pinkcircleofcodeblock]">Education</a>
+                    <a  className="text-[---tw] cursor-pointer hover:text-[---pinkcircleofcodeblock]">Experience</a>
+                    <a  className="text-[---tw] cursor-pointer hover:text-[---pinkcircleofcodeblock]">Blogs</a>
                     </div>
             </nav>
         </div>
     </div>
+    <div className="h-[4rem]"></div>
     </>)
 }
 export default Navbar;
