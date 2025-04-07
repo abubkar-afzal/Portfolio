d9a91c34fce6190c227f91ac1ab7c6960d82b09b
Abubakar <151372655+abubkar-afzal@users.noreply.github.com>
2025-04-07 20:07:59 +0500

    changing in meta tags
    Making changing in meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -11,23 +11,23 @@ export default function App({ Component, pageProps }) {
     <>
     
     <Head>
-        <meta name="keywords" content="Portfolio Of Hafiz Abubakar Afzal - Web Developer" />
+        <meta name="keywords" content="Portfolio Of Hafiz Abubakar Afzal - Full Stack Developer" />
         <meta
           name="description"
-          content="This is the portfolio of Hafiz Abubakar Afzal. I am a web developer and I love to build web applications."
+          content="This is the portfolio of Hafiz Abubakar Afzal. I am a Full Stack Developer and I love to build web applications."
         />
         <meta name="author" content="Hafiz Abubakar Afzal" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <meta property="og:title" content="Portfolio Of Hafiz Abubakar Afzal - Web Developer"></meta>
+        <meta property="og:title" content="Portfolio Of Hafiz Abubakar Afzal - Full Stack Developer"></meta>
         <meta
           property="og:description"
-          content="This is the portfolio of Hafiz Abubakar Afzal. I am a web developer and I love to build web applications."
+          content="This is the portfolio of Hafiz Abubakar Afzal. I am a Full Stack Developer and I love to build web applications."
         ></meta>
-        <meta property="og:image" content="./mypiclong.jpeg"></meta>
+        <meta property="og:image" content="../public/mypiclong.png"></meta>
 
         <meta name="theme-color" content="" />
         <link rel="icon" href="./mypic.png" />
-        <title>Portfolio Of Hafiz Abubakar Afzal - Web Developer</title>
+        <title>Portfolio Of Hafiz Abubakar Afzal - Full Stack Developer</title>
       </Head>
       <div className="fixed z-10 bottom-0 right-0 p-4">
         <FaArrowAltCircleUp onClick={handleScrollToTop} className="opacity-[60%] sm:text-[2rem] text-[3rem] text-[---tw] cursor-pointer hover:scale-[1.2] duration-[2s]"/>
